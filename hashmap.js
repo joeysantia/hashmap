81d63673c1934fbbddce6e9040661b28bc010580
Joey Santia <joey.santia@gmail.com>
2025-01-24 15:26:09 -0500

    Clean up formatting

--- hashmap.js
+++ hashmap.js
@@ -1,185 +1,185 @@
 export default class HashMap {
-    constructor(capacity = 5) {
-        this.buckets = [];
-        this.size = 0; 
-        this.capacity = capacity
+  constructor(capacity = 5) {
+    this.buckets = [];
+    this.size = 0;
+    this.capacity = capacity;
+  }
+
+  get(key) {
+    let cur = this.buckets[this.hash(key)];
+
+    while (cur) {
+      if (cur.key === key) {
+        return cur.value;
+      }
+
+      cur = cur.next;
     }
 
-    get(key) {
-        let cur = this.buckets[this.hash(key)]
+    return null;
+  }
 
-        while (cur) {
-            if (cur.key === key) {
-                return cur.value
-            }
+  set(key, value) {
+    let cur = this.buckets[this.hash(key)];
 
-            cur = cur.next
-        }
-
-        return null 
+    while (cur) {
+      if (cur.key === key) {
+        cur.value = value;
+        return;
+      }
     }
 
-    set(key, value) {
-        let cur = this.buckets[this.hash(key)]
-
-        while (cur) {
-            if (cur.key === key) {
-                cur.value = value 
-                return; 
-            }       
-        }
-
-        this.size++
-        if (this.isAtCapacity()) {
-            this.resize(this.capacity * this.size)
-        }
-
-        const idx = this.hash(key);
-        this.buckets[idx] = new Node(key, value, this.buckets[idx]);
+    this.size++;
+    if (this.isAtCapacity()) {
+      this.resize(this.capacity * this.size);
     }
 
-    has(key) {
-        if (this.size === 0) return false 
-        let cur = this.buckets[this.hash(key)]
+    const idx = this.hash(key);
+    this.buckets[idx] = new Node(key, value, this.buckets[idx]);
+  }
 
-        while (cur) {
-            if (cur.key === key) return true; 
+  has(key) {
+    if (this.size === 0) return false;
+    let cur = this.buckets[this.hash(key)];
 
-            cur = cur.next 
-        }
+    while (cur) {
+      if (cur.key === key) return true;
 
-        return false 
+      cur = cur.next;
     }
 
-    remove(key) {
-        if (!this.has(key)) return; 
+    return false;
+  }
 
-        this.size--;
-        if (this.isNearEmpty()) {
-            this.resize(this.size / this.capacity)
-        }
-        
-        let cur = this.buckets[this.hash(key)]
-        if (cur.key === key) {
-            this.buckets[this.hash(key)] = cur.next;
-            return;
-        }
+  remove(key) {
+    if (!this.has(key)) return;
 
-        while (cur.next) {
-            if (cur.next.key === key) {
-                cur.next = cur.next.next 
-                return; 
-            }
-
-            cur = cur.next; 
-        }
+    this.size--;
+    if (this.isNearEmpty()) {
+      this.resize(this.size / this.capacity);
     }
 
-    length() {
-        return this.size 
+    let cur = this.buckets[this.hash(key)];
+    if (cur.key === key) {
+      this.buckets[this.hash(key)] = cur.next;
+      return;
     }
 
-    clear() {
-        this.buckets = []
-        this.size = 0; 
+    while (cur.next) {
+      if (cur.next.key === key) {
+        cur.next = cur.next.next;
+        return;
+      }
+
+      cur = cur.next;
+    }
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = [];
+    this.size = 0;
+  }
+
+  keys() {
+    let keys = [];
+
+    for (const head of this.buckets) {
+      let cur = head;
+
+      while (cur) {
+        keys.push(cur.key);
+
+        cur = cur.next;
+      }
     }
 
-    keys() {
-        let keys = []
+    return keys;
+  }
 
-        for (const head of this.buckets) {
-            let cur = head; 
+  values() {
+    let values = [];
 
-            while (cur) {
-                keys.push(cur.key)
+    for (const head of this.buckets) {
+      let cur = head;
 
-                cur = cur.next;
-            }
-        }
-
-        return keys 
+      while (cur) {
+        values.push(cur.value);
+        cur = cur.next;
+      }
     }
 
-    values() {
-        let values = []
+    return values;
+  }
 
-        for (const head of this.buckets) {
-            let cur = head;
+  entries() {
+    let entries = [];
 
-            while (cur) {
-                values.push(cur.value)
-                cur = cur.next 
-            }
-        }
+    for (const head of this.buckets) {
+      let cur = head;
 
-        return values 
+      while (cur) {
+        entries.push({ key: cur.key, value: cur.value });
+        cur = cur.next;
+      }
     }
 
-    entries() {
-        let entries = []
+    return entries;
+  }
 
-        for (const head of this.buckets) {
-            let cur = head; 
+  isAtCapacity() {
+    return this.size / this.buckets.length >= this.capacity;
+  }
 
-            while (cur) {
-                entries.push({key: cur.key, value: cur.value})
-                cur = cur.next 
-            }
-        }
+  isNearEmpty() {
+    return this.size / this.buckets.length <= 1 / this.capacity;
+  }
 
-        return entries
+  resize(capacity) {
+    const entries = this.entries();
+    this.buckets = new Array(capacity);
+
+    for (const { key, value } of entries) {
+      this.set(key, value);
     }
+  }
 
-    isAtCapacity() {
-        return this.size / this.buckets.length >= this.capacity
+  hash(key) {
+    const n = this.buckets.length;
+    if (typeof key === "function" || typeof key === "symbol") {
+      throw new Error("Invalid key type");
     }
+    if (typeof key === "number") return key % n;
+    if (typeof key === "bigint") return key % BigInt(n);
 
-    isNearEmpty() {
-        return this.size / this.buckets.length <= (1 / this.capacity)
+    if (typeof key === "boolean") return Number(key) % n;
+    if (typeof key === "string") {
+      let hash = 0;
+
+      for (let i = 0; i < key.length; i++) {
+        hash = 31 * hash + key.charCodeAt(i);
+      }
+      return hash % n;
     }
+    if (typeof key === "object") {
+      let hash = BigInt(0);
 
-    resize(capacity) {
-        const entries = this.entries()
-        this.buckets = new Array(capacity)
+      for (const k in key) {
+        hash += BigInt(this.hash(key[k]));
+      }
 
-        for (const {key, value} of entries) {
-            this.set(key, value)
-        }
+      return hash % BigInt(n);
     }
-
-    hash(key) {
-        const n = this.buckets.length 
-        if (typeof key === 'function' || typeof key === 'symbol') {
-            throw new Error("Invalid key type")
-        }
-        if (typeof key === 'number') return key % n;
-        if (typeof key === 'bigint') return key % BigInt(n); 
-
-        if (typeof key === 'boolean') return Number(key) % n;
-        if (typeof key === 'string') {
-            let hash = 0;
-
-            for (let i = 0; i < key.length; i++) {
-                hash = 31 * hash + key.charCodeAt(i);
-            }
-            return hash % n;
-        }
-        if (typeof key === 'object') {
-            let hash = BigInt(0);
-
-            for (const k in key) {
-                hash += BigInt(this.hash(key[k]))
-            }
-
-            return hash % BigInt(n); 
-        }
-    }
+  }
 }
 
 class Node {
-    constructor(key = null, value = null, next = null) {
-        this.key = key; 
-        this.value = value;
-        this.next = next; 
-    }
+  constructor(key = null, value = null, next = null) {
+    this.key = key;
+    this.value = value;
+    this.next = next;
+  }
 }
